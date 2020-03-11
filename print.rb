bbd37105a3a5a9b7560ab96bbdd49a4cdde4819f
Felipe <felipe.tuyama@thetrainline.com>
2020-03-11 13:55:41 +0000

    Display upvotes

--- print.rb
+++ print.rb
@@ -12,12 +12,13 @@ merge_requests.each do |merge_request|
     puts separator
   end
 
+  upvotes = merge_request.upvotes > 0 ? "#{merge_request.upvotes}★".cyan : ''
   mergeable = merge_request.merge_status == 'can_be_merged' ? '✓'.green : '✗'.red
   time_ago = calculate_time_ago(merge_request.created_at)
 
   puts """    #{merge_request.web_url.cyan}
     #{merge_request.author.username.blue} - #{merge_request.title}
-    #{mergeable} #{merge_request.user_notes_count}C    #{time_ago}
+    #{mergeable} #{merge_request.user_notes_count}C    #{time_ago}  #{upvotes}
   """
 end
 
